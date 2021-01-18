5bf2bc4303adbc1fd8cc1ef3491c83fbab80c2dc
Sunny <ousunny91@gmail.com>
2021-01-18 13:29:19 -0500

    Add skeleton for SeriesItem

---
+++ src/components/SeriesItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({}));
+
+const SeriesItem = () => {
+    const classes = useStyles();
+
+    return <div></div>;
+};
+
+export default SeriesItem;
