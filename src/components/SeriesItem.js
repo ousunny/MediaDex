d24930656f47f9cf7ef6c83c89e19cfd70393e48
Sunny <ousunny91@gmail.com>
2021-01-23 13:47:50 -0500

    Override to force all items to be same height

--- src/components/SeriesItem.js
+++ src/components/SeriesItem.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Card, CardContent } from '@material-ui/core';
+import {
+    Typography,
+    Card,
+    CardActionArea,
+    CardMedia,
+    CardContent,
+} from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-    card: {
-        width: '240px',
-    },
-}));
+const useStyles = makeStyles((theme) => ({}));
 
-const SeriesItem = ({ show: { id, title } }) => {
+const SeriesItem = ({ displayDetailView, show: { id, title } }) => {
     const classes = useStyles();
 
+    const handleCardClick = (event) => {
+        displayDetailView(true);
+    };
+
     return (
-        <Card className={classes.card}>
-            <CardContent>
-                <Typography variant="h5">{title}</Typography>
-            </CardContent>
+        <Card style={{ height: '100%' }}>
+            <CardActionArea onClick={handleCardClick}>
+                <CardMedia component="img" height="200" />
+                <CardContent>
+                    <Typography variant="h5">{title}</Typography>
+                </CardContent>
+            </CardActionArea>
         </Card>
     );
 };
