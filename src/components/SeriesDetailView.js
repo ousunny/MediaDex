371deb3362809895d0e781c632e0ee6a8f895b92
Sunny <ousunny91@gmail.com>
2021-01-24 12:16:30 -0500

    Add component for viewing details of a series

---
+++ src/components/SeriesDetailView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, TextField, Grid, MenuItem } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({}));
+
+const SeriesDetailView = () => {
+    const classes = useStyles();
+
+    return <div>SERIES DETAIL VIEW</div>;
+};
+
+export default SeriesDetailView;
